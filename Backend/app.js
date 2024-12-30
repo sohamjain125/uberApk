3cd827d1cd14b0f39030f9fa1c92e9eb06b2a528
Soham <sohamjain125@gmail.com>
2024-12-30 10:27:18 +0530

    map api done

--- Backend/app.js
+++ Backend/app.js
@@ -5,8 +5,9 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 const connectToDb = require("./db/db.js");
-const userRoutes = require("./routes/user.route");
-const captainRoutes = require("./routes/captain.route");
+const userRoutes = require("./routes/user.routes.js");
+const mapsRoutes = require("./routes/maps.routes.js");
+const captainRoutes = require("./routes/captain.routes.js");
 
 connectToDb();
 
@@ -15,12 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.use("/users", userRoutes);
 app.use("/captains", captainRoutes);
+app.use("/maps", mapsRoutes);
 
 module.exports = app;
